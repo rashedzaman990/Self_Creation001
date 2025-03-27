9c4aa40387e153c94c8c198c1e16ae3ec0de556a
rashedzaman990 <rashedzmn1@gmail.com>
2025-03-27 17:28:31 +0600

    this is my first commit

---
+++ src/GitX001.java
@@ -0,0 +1,12 @@
+
+public class GitX001 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("GitX");
+		
+		GitX002 t = new GitX002();
+		t.Original001();
+	}
+
+}
---
+++ src/GitX002.java
@@ -0,0 +1,9 @@
+
+public class GitX002 {
+	
+	public void Original001() {
+		
+		System.out.println("GitX002");
+	}
+
+}
